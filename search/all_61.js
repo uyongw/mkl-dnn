a6cd63e5b444df0537a97a94e597f2da69692ad3
mkl_dnn <mkl_dnn@intel.com>
2017-05-17 20:11:47 +0300

    gh-pages: update documentation to 364fcd8

--- search/all_61.js
+++ search/all_61.js
@@ -1,10 +1,11 @@
 var searchData=
 [
+  ['accum_5fdata_5ftype',['accum_data_type',['../structmkldnn__convolution__desc__t.html#a755505cf62a730a64378562d4a96b4ee',1,'mkldnn_convolution_desc_t::accum_data_type()'],['../structmkldnn__pooling__desc__t.html#a283f1330595d8bc0bd226576654ea372',1,'mkldnn_pooling_desc_t::accum_data_type()'],['../structmkldnn__inner__product__desc__t.html#a998bb7a60862961aa69e791a703ee0e9',1,'mkldnn_inner_product_desc_t::accum_data_type()']]],
   ['alg_5fkind',['alg_kind',['../structmkldnn__convolution__desc__t.html#a9433e7ea4660fdc9ce3602065fd3ee6c',1,'mkldnn_convolution_desc_t::alg_kind()'],['../structmkldnn__pooling__desc__t.html#aa16febdd2a678f5f1d77ed4b2022dd3b',1,'mkldnn_pooling_desc_t::alg_kind()'],['../structmkldnn__lrn__desc__t.html#a8044a2d8506d9432def40dfd96b5ddd6',1,'mkldnn_lrn_desc_t::alg_kind()']]],
   ['algorithm',['algorithm',['../group__cpp__api__memory.html#gae45a07d6121fdd33e310782753178076',1,'mkldnn']]],
   ['any',['any',['../group__cpp__api__memory.html#gga81bcf1ea92d7f98852a2c3e187825de6af65460ef562d540de287f58445d54748',1,'mkldnn::engine::any()'],['../group__cpp__api__memory.html#gga563b90355ae4fbfed12f6db8ab25a87ea986f069b593c7c6610bc15d678e653d9',1,'mkldnn::memory::any()'],['../group__cpp__api__memory.html#gga7b46a4cd837ea0ccf835d7fa4fc37cffa488e6cebf53697050c0b2cc3829aeb94',1,'mkldnn::stream::any()']]],
-  ['at',['at',['../structmkldnn_1_1primitive_1_1at.html',1,'mkldnn::primitive']]],
   ['at',['at',['../group__cpp__api__memory.html#ga1cd6d53f6696650f834559d2b28d1d5d',1,'mkldnn::primitive::at']]],
+  ['at',['at',['../structmkldnn_1_1primitive_1_1at.html',1,'mkldnn::primitive']]],
   ['auxiliary_20types_20for_20memory_20description',['Auxiliary types for memory description',['../group__c__api__types__memory.html',1,'']]],
   ['a_20performance_20library_20for_20deep_20learning',['A Performance Library for Deep Learning',['../index.html',1,'']]]
 ];
--- search/all_75.js
+++ search/all_75.js
@@ -1,6 +1,7 @@
 var searchData=
 [
   ['utils',['Utils',['../group__cpp__api__utils.html',1,'']]],
+  ['u8',['u8',['../group__cpp__api__memory.html#ggaa8a0d49cc7faaceef9d8f55d66bff57ba0ccb6d0e4130dfe94e06635dac6a9b11',1,'mkldnn::memory']]],
   ['undef',['undef',['../group__cpp__api__memory.html#gga6d88ff11a07bae09a5c348d314c5d1d9a8d3ff637e43a48b61ad6d7855c6a9cd1',1,'mkldnn']]],
   ['use_5fglobal_5fstats',['use_global_stats',['../group__cpp__api__memory.html#gga344b5c240a7392585aba615ff4d0d7f1a97fcd7beeed54e37e2718124d0051d5c',1,'mkldnn']]],
   ['use_5fscale_5fshift',['use_scale_shift',['../group__cpp__api__memory.html#gga344b5c240a7392585aba615ff4d0d7f1a67734f09ab5b5b5282855d52c3e62486',1,'mkldnn']]]
