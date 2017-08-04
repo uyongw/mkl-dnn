7032fa699fedf72bcc407547bb3f363ba2d1da6b
mkl_dnn <mkl_dnn@intel.com>
2017-08-04 17:59:46 +0300

    gh-pages: update documentation to d8fea47

--- search/enumvalues_67.js
+++ search/enumvalues_67.js
@@ -5,9 +5,11 @@ var searchData=
   ['gohwi8o',['gOhwi8o',['../group__cpp__api__memory.html#gga563b90355ae4fbfed12f6db8ab25a87eae7df7db47e81d11cd459f53f46928cb9',1,'mkldnn::memory']]],
   ['goihw',['goihw',['../group__cpp__api__memory.html#gga563b90355ae4fbfed12f6db8ab25a87ea19f8852dc26c175f22aed771f3f9614a',1,'mkldnn::memory']]],
   ['goihw16i16o',['gOIhw16i16o',['../group__cpp__api__memory.html#gga563b90355ae4fbfed12f6db8ab25a87eab47a53954142e271e59e8b0b036c7797',1,'mkldnn::memory']]],
+  ['goihw16o',['gOihw16o',['../group__cpp__api__memory.html#gga563b90355ae4fbfed12f6db8ab25a87eace32ebeb1bd36dc01d4a6aedb81505f3',1,'mkldnn::memory']]],
   ['goihw16o16i',['gOIhw16o16i',['../group__cpp__api__memory.html#gga563b90355ae4fbfed12f6db8ab25a87eaa1cc9ef7726a93eaf88e6556bca94f64',1,'mkldnn::memory']]],
   ['goihw8i16o2i',['gOIhw8i16o2i',['../group__cpp__api__memory.html#gga563b90355ae4fbfed12f6db8ab25a87ea380d9e31a9703c3b520c8cc32e6ae8b9',1,'mkldnn::memory']]],
   ['goihw8i8o',['gOIhw8i8o',['../group__cpp__api__memory.html#gga563b90355ae4fbfed12f6db8ab25a87ea11eda3436ae582b8a28cc85adc8433c1',1,'mkldnn::memory']]],
+  ['goihw8o',['gOihw8o',['../group__cpp__api__memory.html#gga563b90355ae4fbfed12f6db8ab25a87ea3026eb87a3233587bee2d15882e21d63',1,'mkldnn::memory']]],
   ['goihw8o16i2o',['gOIhw8o16i2o',['../group__cpp__api__memory.html#gga563b90355ae4fbfed12f6db8ab25a87ea924d233b52d5b5cbad3f00280e7bae61',1,'mkldnn::memory']]],
   ['goihw8o8i',['gOIhw8o8i',['../group__cpp__api__memory.html#gga563b90355ae4fbfed12f6db8ab25a87eacaf364857cd0309c67bb6dff621ee71e',1,'mkldnn::memory']]]
 ];
